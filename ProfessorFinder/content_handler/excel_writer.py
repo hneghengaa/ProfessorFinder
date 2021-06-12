da114bb6e3b390c3353d9af8604ac56513bc6bc4
KujouRinka <kujourinka@gmail.com>
2021-06-12 22:01:49 +0800

    perf: rewrite logic of writing to excel sheet

--- ProfessorFinder/content_handler/excel_writer.py
+++ ProfessorFinder/content_handler/excel_writer.py
@@ -9,14 +9,20 @@ def check():
 
 
 def write_excel(uni_departments):
-    wb = openpyxl.Workbook()
     university = tuple(uni_departments.keys())[0]
+    filename = 'professor_info/' + university + '.xlsx'
+    try:
+        wb = openpyxl.load_workbook(filename)
+    except FileNotFoundError:
+        wb = openpyxl.Workbook()
     for department, ok in uni_departments.items():
         if ok:
             department = department()
             print('processing: ' + university + department.name)
+            if department.name in wb.sheetnames:
+                wb.remove(wb[department.name])
             ws = wb.create_sheet(department.name, 0)
             ws.append(['学校', '院系', '姓名', '邮箱', '主页'])
             for each in department.run():
                 ws.append(each)
-    wb.save('professor_info/' + university + '.xlsx')
+    wb.save(filename)
